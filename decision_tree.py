f3aa8b6ea3f8e7894fb56d85fb7d018818364110
Jiye100 <22jiye.baek@gmail.com>
2025-05-08 19:25:57 -0400

    Started working on printing results

--- decision_tree.py
+++ decision_tree.py
@@ -5,19 +5,25 @@ from sklearn.tree import DecisionTreeClassifier
 import csv
 
 # Get the training data
-df = pd.read_csv("C:\\Users\\wzhyt\\Downloads\\dont_delete\\AIPRACWINES\\wine-label-decoder\\dataset\\NY_OR_WA_wines.csv")
+df = pd.read_csv(r'dataset\NY_OR_WA_wines.csv')
 
 # Assigning category value to integer
 region = set(df['region'])
 grape = set(df['grape'])
 result = set(zip(df['grape_law'], df['region_law'], df['vintage_law']))
+
 region_mp = {value : idx for idx, value in enumerate(region)}
 grape_mp = {value : idx for idx, value in enumerate(grape)}
 result_mp = {tup : str(idx) for idx, tup in enumerate(result)}
+
+idx2result = {idx: tup for tup, idx in result_mp.items()}
+
+
 df['region'] = df['region'].map(region_mp)
 df['grape'] = df['grape'].map(grape_mp)
 df['result'] = df.apply(lambda row: result_mp.get((row['grape_law'], row['region_law'], row['vintage_law']), 'None'), axis=1)
 
+print(df.head())
 # Removing useless columns
 df = df.drop(df.columns[7], axis = 1)
 df = df.drop(df.columns[6], axis = 1)
@@ -25,8 +31,6 @@ df = df.drop(df.columns[5], axis = 1)
 df = df.drop(df.columns[4], axis = 1)
 df = df.drop(df.columns[3], axis = 1)
 df = df.drop(df.columns[0], axis = 1)
-
-# print(df.head())
 
 # Decision Tree
 X = df[['grape', 'region']]
@@ -39,5 +43,13 @@ dtree = dtree.fit(X, Y)
 # Testing decision tree model
 # TODO
 X_new = pd.DataFrame([[2, 21]], columns=['grape', 'region'])
-print(dtree.predict(X_new))
+# print(dtree.predict(X_new))
 
+def predict_law(df):
+    df['region'] = df['region'].map(region_mp)
+    df['grape'] = df['grape'].map(grape_mp)
+    pred = dtree.predict(df)
+
+    print("prediction done")
+
+    return idx2result[pred[0]]
