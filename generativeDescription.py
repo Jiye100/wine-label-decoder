cae19724fd08f91af5c2e53c6567e3775f179d08
Jiye100 <22jiye.baek@gmail.com>
2025-05-14 16:18:49 -0400

    Added code to generate wine labeling law

--- generativeDescription.py
+++ generativeDescription.py
@@ -53,5 +53,40 @@ def generate_wine_description(name, region):
     description = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return description
 
+def generate_wine_law(grape, region, vintage):
+    prompt = (
+        """Explain the labeling requirements for a wine labeled as a 2020 Pinot Noir from the Willamette Valley AVA. Grape law: 0.75, Region law: 0.85, Vintage law: 0.85
+
+          - The wine must contain at least 75% Pinot Noir grapes.
+          - At least 85% of the grapes must be grown in that AVA.
+          - At least 85% of the grapes must be harvested in that year.
+
+          """
+          """Explain the labeling requirements for a wine labeled as a 2023 Merlot from New Zealand. Grape law: 0.85, Region law: 0.85, Vintage law: 0.85
+
+          - The wine must contain at least 85% Merlot grapes.
+          - At least 85% of the grapes must be grown in that region.
+          - At least 85% of the grapes must be harvested in that year.
+
+          """
+        f"Explain the labeling requirements for a wine labeled as a {vintage} {grape} from the {region}. Grape law: 0.85, Region law: 0.85, Vintage law: 0.85"
+    )
+
+    # Run the model here
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    outputs = model.generate(
+      input_ids,
+      max_length=500,
+      min_length=50, # How long do you want to generate a wine description
+      num_beams=4,
+      no_repeat_ngram_size=3,
+      repetition_penalty=2.5,
+      early_stopping=True
+    )
+    description = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return description
+
+
 # Uncomment this and run this file to specifically testing generative description code
-print(generate_wine_description("Château Margaux 2015", "Bordeaux, France"))
+# print(generate_wine_description("Château Margaux 2015", "Bordeaux, France"))
+print(generate_wine_law("Cabernet Sauvignon", "Californian AVA","2022"))
